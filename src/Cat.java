a4f881539fba14ecbbc0e6c869bbc9e70b171b8e
AndrewPlute <andrew_plute@mines.edu>
2025-02-16 08:54:30 -0700

    Added to github, Created left and right ears

--- src/Cat.java
+++ src/Cat.java
@@ -20,6 +20,12 @@ public class Cat {
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
 	
+	// pick nose dimensions and position
+	private static final int NOSE_WIDTH = 10;
+    private static final int NOSE_HEIGHT = 10;
+    private static final int NOSE_X = HEAD_DIMENSION / 2 - NOSE_WIDTH / 2;
+    private static final int NOSE_Y = HEAD_DIMENSION / 2 - NOSE_HEIGHT / 2;
+	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
 	{
@@ -44,6 +50,22 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);
+		
+		// Draw the left 
+		g2.setColor(Color.black);
+		Polygon leftEar = new Polygon();
+        leftEar.addPoint(catX , catY + 40);   
+        leftEar.addPoint(catX + 20, catY - 20); 
+        leftEar.addPoint(catX + 40, catY + 10);
+        g2.fillPolygon(leftEar);
+
+        // Draw the right ear
+        // Still using black
+        Polygon rightEar = new Polygon();
+        rightEar.addPoint(catX + HEAD_DIMENSION - 40, catY + 10);
+        rightEar.addPoint(catX + HEAD_DIMENSION - 30, catY - 20); 
+        rightEar.addPoint(catX + HEAD_DIMENSION, catY + 40); 
+        g2.fillPolygon(rightEar);
 	}
 }
